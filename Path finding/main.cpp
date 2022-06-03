d619ccad064257a29faa4a9b8f812fa293f78105
Roland Teslaru <89258659+RolandTeslaru@users.noreply.github.com>
2022-06-03 21:14:30 +0300

    Initial Commit

---
+++ Path finding/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Path finding
+//
+//  Created by Roland Teslaru on 03.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
